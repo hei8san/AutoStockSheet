f68c30df282366ee4b71712b7030f8e255a84f07
Kohei <1lovehamuhamu.15@gmail.com>
2024-06-05 18:17:22 +0900

    Add files via upload

---
+++ data_management.py
@@ -0,0 +1,39 @@
+import gspread as gc
+from datetime import date
+
+
+
+class DataManger:
+    def __init__(self, data: dict) -> None:
+        self.data = data
+        self.dict_gid = {"NVDA": 228254102, "TPR": 1833218885, "MSFT": 168617616, "QQQ": 600061735, "^GSPC": 36600606, "GS": 821506004, "GOOG": 2116979974, "AMZN": 1678571736, "LLY": 1757361579 }
+
+    def to_spreadsheet(self, ticker):
+        client = gc.service_account(filename='./sheet-yfinance-per-graph-cca9c444edb4.json')
+        wb_1 = client.open('WB1')
+        sheet = wb_1.get_worksheet_by_id(self.dict_gid[ticker])
+        # dd/mm/YY
+        next_row = len(sheet.get_all_values()) + 1
+        today = date.today()
+        d1 = today.strftime("%Y/%m/%d")
+        sheet.update_cell(next_row,1, d1)
+        try:
+            # stock_price
+            stock_price = self.data['stock_price']
+            sheet.update_cell(next_row,2, stock_price)
+            # per
+            per = self.data['per']
+            sheet.update_cell(next_row,3, per)
+            # eps
+            eps = self.data['eps']
+            sheet.update_cell(next_row,4, eps)
+        except:
+            print("Error occurs")
+            
+        
+        
+        
+        
+        
+        
+
---
+++ main.py
@@ -0,0 +1,44 @@
+import yfinance as yf
+import base64
+from data_management import DataManger
+
+
+def retrieve_data(ticker) -> dict:
+    stock = yf.Ticker(ticker)
+    data = stock.info
+    per = data['trailingPE']
+    eps = data['trailingEps']
+    stock_price = data['currentPrice']
+    result = {"ticker": ticker, "per": per, "eps": eps, "stock_price": stock_price}
+    return result
+
+
+
+
+def hello_pubsub(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    try:
+        if 'data' in event:
+            pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+            # Further processing...
+        else:
+            print("Error: No data key present in the event.")
+    except KeyError as e:
+        print(f"KeyError: {str(e)} - Check the structure of the event object.")
+    except base64.binascii.Error as e:
+        print(f"Base64 Decode Error: {str(e)} - Check if the data is correctly base64 encoded.")
+    except UnicodeDecodeError as e:
+        print(f"Unicode Decode Error: {str(e)} - The decoded data might not be in UTF-8 format.")
+    
+    lst_stocks = ["NVDA", "TPR", "MSFT", "GS", "GOOG", "AMZN", "LLY"]
+    for stock in lst_stocks:
+        try:
+            dict_data = retrieve_data(stock)
+            obj_dataManagement = DataManger(dict_data)
+            obj_dataManagement.to_spreadsheet(stock)
+        except Exception as e:
+            print(f"Error updating spreadsheet for {stock}: {e}")
